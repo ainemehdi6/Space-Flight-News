00b726c4e25959243724b89dc8bcb99bbf81c2f9
EL AINE El Mehdi <ainemehdi6@gmail.com>
2023-02-07 16:09:50 +0100

    Integrating React Native with Linters

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import React from 'react';
 import { LoginScreen } from './src/screens/LoginScreen';
 import { TermsScreen } from './src/screens/TermsScreen';
 import { StarshipFeedScreen } from './src/screens/StarshipFeedScreen';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 const App = () => {
   //return <LoginScreen />;
   //return <TermsScreen />;
-  return <StarshipFeedScreen />;
+  return <QueryClientProvider client={queryClient}><StarshipFeedScreen /></QueryClientProvider>
 };
 
 export default App;
---
+++ src/screens/Offline.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
+import { useIsConnected } from 'react-native-offline';
+
+export const Offline = () => {
+  const isConnected = useIsConnected();
+  return (
+    <View>
+      {isConnected ? (
+        <Text>Connected</Text>
+      ) : (
+        <Text>Offline, reconnection in progress</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginHorizontal: 20,
+    position: "absolute",
+    top: 55,
+    width: "90%",
+    borderRadius: 10,
+    backgroundColor: "#FEE2E2",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  message: {
+    color: "#991B1B",
+  },
+});
