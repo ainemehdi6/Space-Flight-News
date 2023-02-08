14fc7bdfe03b478bece36eca7981cd11e7618222
EL AINE El Mehdi <ainemehdi6@gmail.com>
2023-02-08 13:17:23 +0100

    wrap NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 // App.tsx
 import React from 'react';
-import { LoginScreen } from './src/screens/LoginScreen';
-import { ArticlesScreen } from './src/screens/ArticlesFeedScreen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Navigator from './src/navigation/Navigator';
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,12 +11,13 @@ const queryClient = new QueryClient()
 const App = () => {
   //return <LoginScreen />;
   //return <TermsScreen />;
-  //return <QueryClientProvider client={queryClient}><ArticlesScreen /></QueryClientProvider>
-  <NavigationContainer>
-    <SafeAreaProvider>
-      <Navigator />
-    </SafeAreaProvider>
-  </NavigationContainer>
+  <QueryClientProvider client={queryClient}>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Navigator />
+      </SafeAreaProvider>
+    </NavigationContainer>
+  </QueryClientProvider>
 
 };
 
